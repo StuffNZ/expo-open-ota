12e5d0f52a3b705ab3ecf311483eb66fea54f2c2
proverbiostuff <juampa.proverbio@stuffdigital.co.nz>
2026-07-07 17:58:39 +1200

    [SVS-4611] feat: Add cross account permission

--- infrastructure/shared/ecr.tf
+++ infrastructure/shared/ecr.tf
@@ -31,3 +31,29 @@ resource "aws_ecr_lifecycle_policy" "stuff_expo_ota" {
     ]
   })
 }
+
+# Cross-account pull: the application clusters run in the nebula nonprod/prod
+# accounts and pull this image from the shared-services registry.
+resource "aws_ecr_repository_policy" "cross_account_pull" {
+  repository = aws_ecr_repository.stuff_expo_ota.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCrossAccountPull"
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::781247136068:root", # nebula non-prod
+            "arn:aws:iam::522778376395:root", # nebula prod
+          ]
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+        ]
+      }
+    ]
+  })
+}
